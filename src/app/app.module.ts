3e57039e85b813d3d9ccb55b7cce246c12727b65
KaisarOrange <alif.ayodya@gmail.com>
2023-01-13 11:13:39 +0700

    firts commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    WelcomeComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'menu', component: MenuComponent },
+      { path: 'order', component: WelcomeComponent },
+      { path: '', redirectTo: '/order', pathMatch: 'full' },
+      { path: '**', component: PageNotFoundComponent },
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
